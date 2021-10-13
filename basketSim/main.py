1d9b88b4c85eb777c78d73045356495140844956
77mrt <92452148+77mrt@users.noreply.github.com>
2021-10-13 13:42:53 -0400

    Adding team generation

--- basketSim/main.py
+++ basketSim/main.py
@@ -35,16 +35,27 @@ class Person:
             print(self.name + ' does not have a ball!')
 
 
-def genPlayer():
+def genPlayer(team):
     randThrow = random.randint(5)
     randDefense = random.randint(5)
     f = open(r'C:\Users\liter\Documents\GitHub\vault-of-code\basketSim\playernames.txt')
     name = f.readlines()
-    randpick = random.randint(5)
-    myPerson = Person(name[randpick].rstrip(), 'off_team', True, randThrow, randDefense)
+    randpick = random.randint(len(name))
+    myPerson = Person(name[randpick].rstrip(), team, True, randThrow, randDefense)
     f.close()
     return myPerson
 
+def genTeam():
+    f = open(r"C:\Users\liter\Documents\GitHub\vault-of-code\basketSim\teamnames.txt")
+    team = f.readlines()
+    randpick = random.randint(len(team))
+    i = 0
+    while i < 3:
+        genPlayer(team[randpick].rstrip())
+        i += 1
+        # Complete this function by saving the created player to a new txt file named after the selected team name.
+        # If name already exists, overwrite that file.
+    f.close()
 
 def hoopsGame(Person, rounds):
     player = Person
@@ -78,7 +89,7 @@ def hoopsGame(Person, rounds):
             print('Disapointing')
 
 def main():
-    player = genPlayer()
+    player = genPlayer('off_team')
     print(player.name + ' enters the Court.')
 
     ##hoopsGame(player,10)

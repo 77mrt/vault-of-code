46578e06757e508b50c4dda0551f53668eb0b6fb
77mrt <92452148+77mrt@users.noreply.github.com>
2021-10-26 12:13:03 -0400

    Added team generation

--- basketSim/main.py
+++ basketSim/main.py
@@ -15,6 +15,9 @@ class Person:
     def __str__(self):
         return 'Player: {} \nThrowing: {}'.format(self.name,self.throwStat)
 
+    def savePlayer(self):
+        return '{} {} {} {} {}'.format(self.name,self.team,self.hasBall,self.throwStat,self.defenseStat)
+
     def throwBall(self):
         success = random.randint(100)
         throwChance = 45 + (self.throwStat * 5)
@@ -41,7 +44,7 @@ def genPlayer(team):
     f = open(r'C:\Users\liter\Documents\GitHub\vault-of-code\basketSim\playernames.txt')
     name = f.readlines()
     randpick = random.randint(len(name))
-    myPerson = Person(name[randpick].rstrip(), team, True, randThrow, randDefense)
+    myPerson = Person(name[randpick].rstrip(), team, False, randThrow, randDefense)
     f.close()
     return myPerson
 
@@ -49,19 +52,27 @@ def genTeam():
     f = open(r"C:\Users\liter\Documents\GitHub\vault-of-code\basketSim\teamnames.txt")
     team = f.readlines()
     randpick = random.randint(len(team))
+    tName = team[randpick].rstrip()
+    newFile = "C:/Users/liter/Documents/GitHub/vault-of-code/basketSim/teamslog/" + tName + ".txt"
     i = 0
+    t = open(newFile, 'w')
     while i < 3:
-        genPlayer(team[randpick].rstrip())
+        addPlayer = genPlayer(tName)
+        t.write(addPlayer.savePlayer()+"\n")
         i += 1
-        # Complete this function by saving the created player to a new txt file named after the selected team name.
-        # If name already exists, overwrite that file.
+    t.close()
+    t2 = open(newFile,'r')
+    roster = t2.readlines()
     f.close()
+    print(roster)
+    return "The " + tName +"\n"
 
 def hoopsGame(Person, rounds):
     player = Person
     score = 0
     misses = 0
     cont = 0
+    player.hasBall = True
     while cont < rounds:
         if player.hasBall == True:
             print(player.name + ' has the ball!')
@@ -79,22 +90,26 @@ def hoopsGame(Person, rounds):
         print('Misses: ' + str(misses))
         player.hasBall = True
         cont += 1
+        sleep(3)
         print('')
 
-        if score < misses:
-            print(player.name + ' loses!')
-        elif score > misses:
-            print(player.name + ' wins!')
-        else:
-            print('Disapointing')
+    if score < misses:
+        print(player.name + ' loses!')
+    elif score > misses:
+        print(player.name + ' wins!')
+    else:
+        print('Disapointing')
 
 def main():
-    player = genPlayer('off_team')
-    print(player.name + ' enters the Court.')
+    #player = genPlayer('off_team')
+    #print(player.name + ' enters the Court.')
 
-    ##hoopsGame(player,10)
+    #hoopsGame(player,10)
 
-    print(player.__str__())
+    #print(player.__str__())
+
+    team = genTeam()
+    print(team)
 
 if __name__ == '__main__':
     main()
